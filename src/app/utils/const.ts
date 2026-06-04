9fd46c2aa8fd1cd1334625b67a46281782776f2c
ruship13 <126866397+ruship13@users.noreply.github.com>
2026-06-04 12:33:18 +0530

    Update const.ts

--- src/app/utils/const.ts
+++ src/app/utils/const.ts
@@ -9,7 +9,7 @@ export function getBaseUrl(): string {
   if (origin.includes('10.192.65.167')) {
     return 'http://10.192.65.167:8080/ats-mahindra-battery-0.0.1/';
   } else if (origin.includes('192.168.11.76')) {
-    return 'http://http://192.168.11.76:8088/ats-mahindra-battery-0.0.1/';
+    return 'http://http://192.168.11.76:8088/ats-mahindra-battery-0.0.1-SNAPSHOT/';
   } else if (origin.includes('localhost')) {
     return 'http://localhost:8082/';
   }
